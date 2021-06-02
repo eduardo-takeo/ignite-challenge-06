8be7577b90beb8bc18096e971742dc62fd93e677
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-02 00:27:58 -0300

    Create Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Flex, Image, Link } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
 
 export function Header() {
   return (
-    <Flex as="header">
-      <Button>back</Button>
+    <Flex as="header" w="100%" h="24" align="center" justify="center">
+      <Flex
+        w="100%"
+        maxWidth={1280}
+        align="center"
+        justify="space-between"
+        px="4"
+      >
+        <Link href="/">
+          <ChevronLeftIcon fontSize={30} />
+        </Link>
+        <Image src="/logo.svg" alt="Logo" w="44" />
+        {/* TODO: See a better way to style */}
+        <Box />
+      </Flex>
     </Flex>
   );
 }
