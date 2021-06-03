2092a2c4c89274542380007b6ba9360fd6e32171
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-06-03 16:29:58 -0300

    Remove back button when Home is the current page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,22 @@
-import { Box, Flex, Image, Link } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { Flex, Image, Link } from "@chakra-ui/react";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 
 export function Header() {
+  const { pathname } = useRouter();
+
   return (
     <Flex as="header" w="100%" h="24" align="center" justify="center">
-      <Flex
-        w="100%"
-        maxWidth={1280}
-        align="center"
-        justify="space-between"
-        px="4"
-      >
-        <Link href="/">
-          <ChevronLeftIcon fontSize={30} />
+      <Flex w="100%" maxWidth={1280} align="center" px="4">
+        {pathname !== "/" && (
+          <Link href="/">
+            <ChevronLeftIcon fontSize={30} />
+          </Link>
+        )}
+        <Link href="/" mx="auto">
+          <Image src="/icons/logo.svg" alt="Logo" w="44" />
         </Link>
-        <Image src="/icons/logo.svg" alt="Logo" w="44" />
-        <Box />
       </Flex>
     </Flex>
   );
 }
-
-//TODO:
-//* Add link to icon
-//* Hide back button when current page is Home
